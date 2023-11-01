d2b9ae8c8901b0aba8172f19388363413a841f1f
GVME707 <shironekomemeow@gmail.com>
2023-11-01 14:30:20 +0700

    debugging

--- src/app/page.js
+++ src/app/page.js
@@ -5,15 +5,29 @@ export default function Home() {
   const [users, setUsers] = useState([]);
 
   useEffect(() => {
-    console.log(process.env.NEXT_PUBLIC_API_URL);
-    fetch(process.env.NEXT_PUBLIC_API_URL + "/users")
+    const apiUrl = process.env.NEXT_PUBLIC_API_URL;
+    console.log("API URL from env:", apiUrl);  // Debugging line
+
+    if (!apiUrl) {
+      console.error("API URL is not set!");  // Debugging line
+      return;
+    }
+
+    const fetchUrl = `${apiUrl}/users`;
+    console.log("Fetching data from:", fetchUrl);  // Debugging line
+
+    fetch(fetchUrl)
       .then((response) => {
+        console.log("Fetch response:", response);  // Debugging line
         if (!response.ok) {
           throw new Error("Network response was not ok " + response.statusText);
         }
         return response.json();
       })
-      .then((json) => setUsers(json))
+      .then((json) => {
+        console.log("Received JSON data:", json);  // Debugging line
+        setUsers(json);
+      })
       .catch((error) => {
         console.error(
           "There has been a problem with your fetch operation:",
@@ -26,14 +40,18 @@ export default function Home() {
     <main>
       <h1>React App</h1>
       <div>
-        {users.map((user) => (
-          <div key={user.id}>
-            <li>
-              <h2>{user.name}</h2>
-              <p>{user.email}</p>
-            </li>
-          </div>
-        ))}
+        {users.length === 0 ? (  // Debugging line
+          <div>No users found</div>
+        ) : (
+          users.map((user) => (
+            <div key={user.id}>
+              <li>
+                <h2>{user.name}</h2>
+                <p>{user.email}</p>
+              </li>
+            </div>
+          ))
+        )}
       </div>
     </main>
   );
